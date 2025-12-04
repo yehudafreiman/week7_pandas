8742d049515510197c2d191b6457543cb7fcb8a8
Yehuda Freiman <yehfre@icloud.com>
2025-12-04 11:26:59 +0200

    Clean and process order data from JSON to CSV
    This script loads a JSON file, cleans the data by replacing certain values, converts data types, creates new columns, and exports the cleaned data to a CSV file.

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pd
+
+from utils import load_json
+
+if __name__ == "__main__":
+
+    # load json
+    df = load_json("orders_simple.json")
+
+    # cleaning data
+    df["total_amount"] = df["total_amount"].str.replace('$', '')
+
+    df["items_html"] = df["items_html"].str.replace('<b>', '')
+    df["items_html"] = df["items_html"].str.replace('</b>', ' ')
+    df["items_html"] = df["items_html"].str.replace('<br>', '')
+
+    df["coupon_used"] = df["coupon_used"].replace({'': 'no coupon'})
+
+    # converse data type
+    df["total_amount"] = df["total_amount"].astype(float)
+    df["order_date"] = pd.to_datetime(df["order_date"])
+
+    # create column order_month
+    df["order_month"] = df['order_date'].dt.month
+
+    # create column high_value_order
+    average_total_amount = df["total_amount"].mean()
+    df["high_value_order"] = [True if x > average_total_amount else False for x in df["total_amount"]]
+
+    # sort df by total_amount
+    df = df.sort_values("total_amount", ascending=False)
+
+    # create column rating_average
+    rating_average_by_country = df.groupby("country")["rating"].mean()
+    df["rating_average"] = df.groupby("country")["rating"].transform('mean')
+
+    
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    # export to csv
+    df.to_csv("clean_orders_205368319.csv")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+   
